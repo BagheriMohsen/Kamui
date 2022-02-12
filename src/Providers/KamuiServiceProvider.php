d19860b661e715fef3a1b37fb9e5abe2c87141d9
Mohsen Bagheri <bagherimohsenbagheri@gmail.com>
2022-02-12 20:56:21 +0330

    initial commit

---
+++ src/Providers/KamuiServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Mohsenbagheri\Kamui\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class KamuiServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
